9b0ebf9d6a2f95b3a3ae7b451b3566351a0a9c9d
FerreiraMF <mf035033@gmail.com>
2026-08-08 01:34:47 -0300

    criação da função "clearInputs"

--- script.js
+++ script.js
@@ -5,13 +5,65 @@ const addButton = document.querySelector('button');
 
 const transactionList = document.querySelector('.transaction-list');
 
-const transactions = [ 
-{
-    description: 'Exemplo de transação',
-    amount: 100,
-    type: 'income',
-    date: new Date(),
-    id: 1,
+const transactions = [];
+
+function addTransaction() {
+
+    console.log('AddTransaction executou');
+ // Get input values
+    const description = descriptionInput.value;
+    const amount = parseFloat(amountInput.value);
+    const type = typeSelect.value;
+ // validate inputs
+    if (!description || isNaN(amount) || amount <= 0 || !type) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+ // Create transaction object
+    const transaction = {
+        id: transactions.length + 1,
+        description,
+        amount,
+        type,
+        date: new Date(),
+    };
+ // add transaction to transactions array
+    transactions.push(transaction);
+    renderTransactions();
+    console.log('Transação adicionada:', transaction);
+ // Update transaction list
+
+ clearInputs();
 }
 
-];
+function renderTransactions() {
+ transactionList.innerHTML = '';
+    transactions.forEach(transaction =>  {
+        const transactionItem = document.createElement('div');
+            transactionItem.className = 'transaction-card';
+
+        const descriptionElement = document.createElement('h3');
+        const amountElement = document.createElement('p');
+        const typeElement = document.createElement('p');
+        const dateElement = document.createElement('p');
+
+        descriptionElement.innerText = transaction.description;
+        amountElement.innerText = `R$ ${transaction.amount.toFixed(2)}`;
+        typeElement.innerText = transaction.type;
+        dateElement.innerText = transaction.date.toLocaleDateString();
+
+        transactionItem.appendChild(descriptionElement);
+        transactionItem.appendChild(amountElement);
+        transactionItem.appendChild(typeElement);
+        transactionItem.appendChild(dateElement);
+        transactionList.appendChild(transactionItem);
+        });
+}
+
+function clearInputs() {
+    descriptionInput.value = '';
+    amountInput.value = '';
+    typeSelect.value = '';
+}
+
+addButton.addEventListener("click", addTransaction);
